9b3488a399e2e60dbb593ca5794b65d07f78b207
sekougit <sekoudrame339@gmail.com>
2026-05-11 12:19:17 +0000

    commentaire sur le yml

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,6 +1,6 @@
-import os
+# import os
 
-print("Traitement données kobo et clients")
-os.system("python scripts/ETL_DATA_FINANCE_KOBO.py")
+# print("Traitement données kobo et clients")
+# os.system("python scripts/ETL_DATA_FINANCE_KOBO.py")
 
-print("Pipeline terminé")
+# print("Pipeline terminé")
